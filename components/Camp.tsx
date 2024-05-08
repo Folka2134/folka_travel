79a06276a7f8ef9740106566f1a5b6d984e676f9
folka2134 <folka2134@gmail.com>
2024-05-08 19:50:06 +0100

    completed hero

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -59,12 +59,12 @@ const Camp = () => {
           subtitle="Prigen, Pasuruan"
           peopleJoined="50+ Joined"
         />
-        <CampSite
+        {/* <CampSite
           backgroundImage="bg-bg-img-2"
           title="Mountain View Camp"
           subtitle="Somewhere in the Wilderness"
           peopleJoined="50+ Joined"
-        />
+        /> */}
       </div>
 
       <div className="flexEnd mt-10 px-6 lg:-mt-60 lg:mr-6">
