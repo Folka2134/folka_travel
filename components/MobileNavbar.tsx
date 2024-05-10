57488cdefd36c67fef54edefc3d19981b2011536
folka2134 <folka2134@gmail.com>
2024-05-10 13:51:32 +0100

    created mobile navbar

---
+++ components/MobileNavbar.tsx
@@ -0,0 +1,77 @@
+import { NAV_LINKS } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import Button from "./Button";
+import {
+  SignInButton,
+  SignOutButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+
+const MobileNavbar = () => {
+  return (
+    <div className="lg:hidden">
+      <Sheet>
+        <SheetTrigger>
+          <Image
+            src="menu.svg"
+            alt="menu"
+            width={32}
+            height={32}
+            className="mr-4 inline-block cursor-pointer lg:hidden"
+          />
+        </SheetTrigger>
+        <SheetClose asChild>
+          <SheetContent className="flex max-w-[200px] flex-col">
+            <SheetHeader className="">
+              <SheetDescription className="mt-16 flex h-full flex-col gap-12">
+                <ul className="flex flex-col">
+                  {NAV_LINKS.map((link) => (
+                    <SheetClose asChild>
+                      <Link
+                        href={link.href}
+                        key={link.key}
+                        className="regular-24 flexCenter cursor-pointer py-6 text-gray-50 transition-all hover:font-bold"
+                      >
+                        {link.label}
+                      </Link>
+                    </SheetClose>
+                  ))}
+                </ul>
+                <SignedOut>
+                  <div className="flexCenter">
+                    <SheetClose asChild>
+                      <Link href="/sign-in">
+                        <button className="rounded-full bg-black px-8 py-2 text-white transition-all hover:bg-[#01AA51] hover:font-bold active:scale-[0.95]">
+                          Login
+                        </button>
+                      </Link>
+                    </SheetClose>
+                  </div>
+                </SignedOut>
+                <SignedIn>
+                  <div className="flexCenter">
+                    <UserButton afterSignOutUrl="/" />
+                  </div>
+                </SignedIn>
+              </SheetDescription>
+            </SheetHeader>
+          </SheetContent>
+        </SheetClose>
+      </Sheet>
+    </div>
+  );
+};
+
+export default MobileNavbar;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,54 +9,58 @@ import {
   SignedOut,
   UserButton,
 } from "@clerk/nextjs";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import MobileNavbar from "./MobileNavbar";
 
 const Navbar = () => {
   return (
-    <nav className="flexBetween max-container padding-container relative z-30 py-5">
-      <Link href="/">
-        <Image
-          className="rounded-xl"
-          src="/folka_travel-logo.png"
-          alt="logo"
-          width={200}
-          height={50}
-        />
-      </Link>
+    <div className="sticky top-6 z-50 mx-4 my-6 flex items-center justify-between rounded-xl bg-white sm:mx-6 md:mx-12 lg:relative">
+      <nav className="flex w-full items-center justify-between">
+        <Link href="/">
+          <Image
+            className="rounded-xl"
+            src="/folka_travel-logo.png"
+            alt="logo"
+            width={200}
+            height={50}
+          />
+        </Link>
 
-      <ul className="hidden h-full gap-12 lg:flex">
-        {NAV_LINKS.map((link) => (
-          <Link
-            href={link.href}
-            key={link.key}
-            className="regular-16 flexCenter cursor-pointer pb-1.5 text-gray-50 transition-all hover:font-bold"
-          >
-            {link.label}
-          </Link>
-        ))}
-      </ul>
-      <SignedOut>
-        <div className="lg:flexCenter hidden">
-          <Link href="/sign-in">
-            <button className="rounded-full bg-black px-8 py-2 text-white transition-all hover:bg-[#01AA51] hover:font-bold active:scale-[0.95]">
-              Login
-            </button>
-          </Link>
-        </div>
-      </SignedOut>
-      <SignedIn>
-        <div className="lg:flexCenter hidden">
-          <UserButton afterSignOutUrl="/" />
-        </div>
-      </SignedIn>
-
-      <Image
-        src="menu.svg"
-        alt="menu"
-        width={32}
-        height={32}
-        className="inline-block cursor-pointer lg:hidden"
-      />
-    </nav>
+        <ul className="hidden h-full gap-12 lg:flex">
+          {NAV_LINKS.map((link) => (
+            <Link
+              href={link.href}
+              key={link.key}
+              className="regular-16 flexCenter cursor-pointer pb-1.5 text-gray-50 transition-all hover:font-bold"
+            >
+              {link.label}
+            </Link>
+          ))}
+        </ul>
+        <SignedOut>
+          <div className="lg:flexCenter hidden">
+            <Link href="/sign-in">
+              <button className="rounded-full bg-black px-8 py-2 text-white transition-all hover:bg-[#01AA51] hover:font-bold active:scale-[0.95]">
+                Login
+              </button>
+            </Link>
+          </div>
+        </SignedOut>
+        <SignedIn>
+          <div className="lg:flexCenter hidden">
+            <UserButton afterSignOutUrl="/" />
+          </div>
+        </SignedIn>
+      </nav>
+      <MobileNavbar />
+    </div>
   );
 };
 
