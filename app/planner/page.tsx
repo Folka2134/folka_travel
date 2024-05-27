e8e7b5e98baaee6cf13fd07c961eab1bbe34876d
folka2134 <folka2134@gmail.com>
2024-05-27 21:46:37 +0100

    pages under construction

---
+++ app/planner/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PlannerPage = () => {
+  return (
+    <div className="my-52 flex justify-center text-3xl font-bold text-red-600">
+      Under Construction
+    </div>
+  );
+};
+
+export default PlannerPage;
---
+++ app/world/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const WorldPage = () => {
+  return (
+    <div className="my-52 flex justify-center text-3xl font-bold text-red-600">
+      Under Construction
+    </div>
+  );
+};
+
+export default WorldPage;
