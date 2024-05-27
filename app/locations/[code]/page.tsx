199ecae0ef72d1fc18b96bed454baa844a6b7db5
folka2134 <folka2134@gmail.com>
2024-05-27 11:03:38 +0100

    added pagination

--- app/locations/[code]/page.tsx
+++ app/locations/[code]/page.tsx
@@ -1,24 +1,57 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
 import { cities } from "@/constants/cities";
 import { countries } from "@/constants/countries";
 
 const LocationPage = () => {
   const params = useParams<{ code: string }>();
-
-  const removedDuplicates = cities.filter(
-    (value: any, index: any, self: any) =>
-      self.map((item: any) => item.name).indexOf(value.name) === index,
-  );
-  const filteredCities = removedDuplicates
-    .filter((city: any) => city.country === params.code)
-    .sort();
-
-  console.log(filteredCities);
+  const [page, setPage] = useState(0);
+  const [data, setData] = useState([]);
 
   const country = countries.find((country) => country.code === params.code);
+
+  const getFromAndTo = () => {
+    const ITEM_PER_PAGE = 10;
+    let from = page * ITEM_PER_PAGE;
+    let to = from + ITEM_PER_PAGE;
+
+    if (page > 0) {
+      from += 1;
+    }
+
+    return { from, to };
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
+    const { from, to } = getFromAndTo();
+
+    console.log(from, to);
+
+    // ALL DATA
+    const removedDuplicates = cities.filter(
+      (value: any, index: any, self: any) =>
+        self.map((item: any) => item.name).indexOf(value.name) === index,
+    );
+
+    const filteredCities = removedDuplicates
+      .filter((city: any) => city.country === params.code)
+      .sort()
+      .slice(from, to);
+
+    setPage(page + 1);
+
+    // console.log(removedDuplicates);
+
+    setData(filteredCities);
+  };
+
+  console.log(data);
 
   return (
     <div className="max-container flex flex-col gap-12 py-10 pb-32  lg:py-20 ">
@@ -27,12 +60,21 @@ const LocationPage = () => {
       </h2>
 
       <div className="grid grid-cols-2 gap-8 sm:grid-cols-3 lg:grid-cols-4">
-        {filteredCities.map((city: any) => (
+        {data?.map((city: any) => (
           <div key={city.name}>
             <h3>{city.name}</h3>
           </div>
         ))}
       </div>
+
+      <button
+        className="btn btn-primary"
+        onClick={() => {
+          fetchData();
+        }}
+      >
+        More...
+      </button>
     </div>
   );
 };
