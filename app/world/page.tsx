6aecb4ab9fbe8b253c4fbb5187f1e2a5a2018ca3
folka2134 <folka2134@gmail.com>
2024-05-28 09:59:00 +0100

    updated under construction sign

--- app/world/page.tsx
+++ app/world/page.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import Image from "next/image";
+
+import underConstruction from "../../public/under_construction.jpg";
 
 const WorldPage = () => {
   return (
-    <div className="my-52 flex justify-center text-3xl font-bold text-red-600">
-      Under Construction
+    <div className="my-24 flex justify-center text-3xl font-bold text-red-600">
+      <Image
+        src={underConstruction}
+        // width={50}
+        // height={50}
+        alt="Under construction"
+      />
     </div>
   );
 };
